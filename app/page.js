7bd832e85dab6462e223c0a6162b67d40d8599b9
Sudo-Self <sudo-self@proton.me>
2024-09-17 13:41:08 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -72,31 +72,17 @@ const Home = () => {
     });
   };
 
-
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground">
-      <script src="https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/scripts%2F1726600191201-im3jje7xy9.js?alt=media&token=446aa933-b311-4569-b16a-7fb0f6be2b71"></script>
       <header className="relative p-6 bg-opacity-30 w-full max-w-screen-2xl mx-auto flex items-center justify-between">
         <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-200 flex-grow">
           Floater B.
         </h1>
         <div className="flex items-center space-x-4">
-          <button
-            onClick={toggleDarkMode}
-            className="p-2 rounded hover:bg-gray-700 transition-colors duration-300"
-          >
-            {darkMode ? (
-              <SunIcon className="w-6 h-6 text-gray-400" />
-            ) : (
-              <MoonIcon className="w-6 h-6 text-gray-400" />
-            )}
+          <button onClick={toggleDarkMode} className="p-2 rounded hover:bg-gray-700 transition-colors duration-300">
+            {darkMode ? <SunIcon className="w-6 h-6 text-gray-400" /> : <MoonIcon className="w-6 h-6 text-gray-400" />}
           </button>
-          <a
-            href="https://github.com/sudo-self/floater-b"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-400 hover:text-gray-300 transition-colors duration-300"
-          >
+          <a href="https://github.com/sudo-self/floater-b" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-300 transition-colors duration-300">
             <FaGithub className="w-6 h-6" />
           </a>
         </div>
@@ -104,7 +90,7 @@ const Home = () => {
 
       <main className="flex-grow">
         <BigText slides={['npx', 'floater-xyz']} />
-        
+
         {!user ? (
           <div className="flex flex-row space-x-4 items-center justify-center p-4">
             <button className="px-4 py-2 bg-blue-700 text-white rounded" onClick={signInWithGoogle}>
@@ -119,91 +105,60 @@ const Home = () => {
             <button className="px-4 py-2 bg-red-600 text-white rounded mb-2" onClick={handleSignOut}>
               Sign Out
             </button>
-            <div className="mb-4">
-              <input
-                type="text"
-                placeholder="enter image URL:"
-                value={bgImageUrl}
-                onChange={(e) => setBgImageUrl(e.target.value)}
-                className="p-2 border-b border-gray-300 bg-transparent text-black dark:border-gray-700 dark:text-white focus:outline-none"
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="text"
-                placeholder="enter website URL:"
-                value={iframeUrl}
-                onChange={(e) => setIframeUrl(e.target.value)}
-                className="p-2 border-b border-gray-300 bg-transparent text-black dark:border-gray-700 dark:text-white focus:outline-none"
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="text"
-                placeholder="enter button name:"
-                value={label}
-                onChange={(e) => setLabel(e.target.value)}
-                className="p-2 border-b border-gray-300 bg-transparent text-black dark:border-gray-700 dark:text-white focus:outline-none"
-              />
-            </div>
-   <button
-  onClick={handleGenerateAndUpload}
-  disabled={!allFilled}
-  className={`px-4 py-2 ${allFilled ? 'bg-green-900' : 'bg-gray-400'} text-white rounded`}
->
-  Create Custom Floater
-</button>
+            <input type="text" placeholder="enter image URL:" value={bgImageUrl} onChange={(e) => setBgImageUrl(e.target.value)} className="p-2 border-b border-gray-300 bg-transparent text-black dark:border-gray-700 dark:text-white focus:outline-none mb-4" />
+            <input type="text" placeholder="enter website URL:" value={iframeUrl} onChange={(e) => setIframeUrl(e.target.value)} className="p-2 border-b border-gray-300 bg-transparent text-black dark:border-gray-700 dark:text-white focus:outline-none mb-4" />
+            <input type="text" placeholder="enter button name:" value={label} onChange={(e) => setLabel(e.target.value)} className="p-2 border-b border-gray-300 bg-transparent text-black dark:border-gray-700 dark:text-white focus:outline-none mb-4" />
 
-{scriptUrl && (
-  <div className="mt-4 flex flex-col items-center">
-    <img
-      onClick={() => {
-        copyToClipboard(`<script src="${scriptUrl}"></script>`);
-      }}
-      src="https://api.iconify.design/ic:outline-copy-all.svg?color=%23929292"
-      alt="Copy icon"
-      className="w-8 h-8 mb-1 cursor-pointer"
-    />
-    <span>{copied ? 'Embed script is ready!' : 'Copy floater button'}</span>
-  </div>
-)}
+            <button onClick={handleGenerateAndUpload} disabled={!allFilled} className={`px-4 py-2 ${allFilled ? 'bg-green-900' : 'bg-gray-400'} text-white rounded`}>
+              Create Custom Floater
+            </button>
 
-        <div className="flex justify-center mt-8">
-          <button
-            onClick={() => setShape(shape === "circle" ? "square" : "circle")}
-            className={`p-2 transition-colors duration-300 text-white border border-gray-950 focus:outline-none
-            ${shape === "circle" ? "bg-green-500" : "bg-gray-800"}
-            ${shape === "circle" ? "rounded-full" : "rounded-none"} relative`}
-            style={{
-              backgroundColor: color || "#4b0082",
-              width: "70px",
-              height: "70px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            title={`Click to toggle shape to ${shape === "circle" ? "square" : "circle"}`}
-          >
-            <span
-              className="absolute inset-0"
-              style={{
-                backgroundColor: hoverColor || "rgba(255, 255, 255, 0.3)",
-                opacity: 0,
-                transition: "opacity 0.3s",
-                zIndex: -1,
-              }}
-            />
-            {label || (shape === "circle" ? "(■_■)" : "[■_■]")}
-          </button>
-        </div>
+            {scriptUrl && (
+              <div className="mt-4 flex flex-col items-center">
+                <img
+                  onClick={() => {
+                    copyToClipboard(`<script src="${scriptUrl}"></script>`);
+                  }}
+                  src="https://api.iconify.design/ic:outline-copy-all.svg?color=%23929292"
+                  alt="Copy icon"
+                  className="w-8 h-8 mb-1 cursor-pointer"
+                />
+                <span>{copied ? 'Embed script is ready!' : 'Copy floater button'}</span>
+              </div>
+            )}
 
-        <div className="flex justify-center mt-4">
-          <ColorPicker
-            setColor={setColor}
-            setHoverColor={setHoverColor}
-            activeColorInput="color"
-          />
-        </div>
+            <div className="flex justify-center mt-8">
+              <button
+                onClick={() => setShape(shape === 'circle' ? 'square' : 'circle')}
+                className={`p-2 transition-colors duration-300 text-white border border-gray-950 focus:outline-none ${shape === 'circle' ? 'bg-green-500' : 'bg-gray-800'} ${shape === 'circle' ? 'rounded-full' : 'rounded-none'} relative`}
+                style={{
+                  backgroundColor: color || '#4b0082',
+                  width: '70px',
+                  height: '70px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                title={`Click to toggle shape to ${shape === 'circle' ? 'square' : 'circle'}`}
+              >
+                <span
+                  className="absolute inset-0"
+                  style={{
+                    backgroundColor: hoverColor || 'rgba(255, 255, 255, 0.3)',
+                    opacity: 0,
+                    transition: 'opacity 0.3s',
+                    zIndex: -1,
+                  }}
+                />
+                {label || (shape === 'circle' ? '(■_■)' : '[■_■]')}
+              </button>
+            </div>
+
+            <div className="flex justify-center mt-4">
+              <ColorPicker setColor={setColor} setHoverColor={setHoverColor} activeColorInput="color" />
+            </div>
+          </div>
+        )}
       </main>
 
       <Footer />
