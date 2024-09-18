ae8cac7bafe39a1d3a05ab279ea81210c3622afb
Sudo-Self <sudo-self@proton.me>
2024-09-18 01:21:00 -0600

    Create clientScriptComponent.js

---
+++ app/clientScriptComponent.js
@@ -0,0 +1,13 @@
+
+"use client";
+
+import Script from 'next/script';
+
+export default function ClientScriptComponent() {
+  return (
+    <Script
+      src="https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/scripts%2F1726642674787-nbl1t6kdlp.js?alt=media&token=e12485cb-0d3a-4a84-ae8c-537fb9718703"
+      strategy="afterInteractive"
+    />
+  );
+}
