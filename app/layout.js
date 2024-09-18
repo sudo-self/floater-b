ccfab778ba8b4a4870648f3d2b6694170be556c5
Sudo-Self <sudo-self@proton.me>
2024-09-18 00:59:27 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
+    <script src="https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/scripts%2F1726642674787-nbl1t6kdlp.js?alt=media&token=e12485cb-0d3a-4a84-ae8c-537fb9718703"></script>      <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
