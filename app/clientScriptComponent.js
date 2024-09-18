0121bb80162f804b89c9664ea5d32ef40109c0aa
Sudo-Self <sudo-self@proton.me>
2024-09-18 01:45:23 -0600

    Update clientScriptComponent.js

--- app/clientScriptComponent.js
+++ app/clientScriptComponent.js
@@ -6,7 +6,7 @@ import Script from 'next/script';
 export default function ClientScriptComponent() {
   return (
     <Script
-      src="https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/scripts%2F1726642674787-nbl1t6kdlp.js?alt=media&token=d7136352-a216-426b-8576-763d12b3e4a3"
+      src="https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/scripts%2F1726642674787-nbl1t6kdlp.js?alt=media&token=a147ba52-2201-4d15-aaf6-5ca08282ee83"
       strategy="afterInteractive"
     />
   );
