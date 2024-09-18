c60ac757e86954f2211e013d469fdc3a9dc8566d
Sudo-Self <sudo-self@proton.me>
2024-09-18 04:18:44 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-
 import localFont from "next/font/local";
 import Head from "next/head";
 import ClientScriptComponent from './clientScriptComponent';
@@ -25,6 +24,21 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Create Custom Embeddable Floater Buttons</title>
+        <meta name="title" content="Create Custom Embeddable Floater Buttons" />
+        <meta name="description" content="Create Custom Embeddable Floater buttons that you can share or host on your site with zero-code" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://floater.jessejesse.xyz" />
+        <meta property="og:title" content="Create Custom Embeddable Floater Buttons" />
+        <meta property="og:description" content="Create Custom Embeddable Floater buttons that you can share or host on your site with zero-code" />
+        <meta property="og:image" content="https://floater.jessejesse.xyz/floater.og" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://floater.jessejesse.xyz" />
+        <meta property="twitter:title" content="Create Custom Embeddable Floater Buttons" />
+        <meta property="twitter:description" content="Create Custom Embeddable Floater buttons that you can share or host on your site with zero-code" />
+        <meta property="twitter:image" content="https://floater.jessejesse.xyz/floater.og" />
+        <meta name="author" content="Floater B." />
       </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
@@ -33,3 +47,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
