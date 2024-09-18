94a9a2c1014bb0ee148820d2240f5980e10c287f
Sudo-Self <sudo-self@proton.me>
2024-09-18 01:21:32 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+
 import localFont from "next/font/local";
 import Head from "next/head";
+import ClientScriptComponent from './clientScriptComponent';
 import "./globals.css";
 
 const geistSans = localFont({
@@ -20,17 +21,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/scripts%2F1726642674787-nbl1t6kdlp.js?alt=media&token=e12485cb-0d3a-4a84-ae8c-537fb9718703";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <html lang="en">
       <Head>
@@ -40,9 +30,11 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <ClientScriptComponent />
       </body>
     </html>
   );
 }
+
 
 
