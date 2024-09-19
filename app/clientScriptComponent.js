a49ca6f1631b1ccec51db7045d14caab6b736d61
Sudo-Self <sudo-self@proton.me>
2024-09-19 00:23:30 -0600

    Update clientScriptComponent.js

--- app/clientScriptComponent.js
+++ app/clientScriptComponent.js
@@ -7,7 +7,7 @@ export default function ClientScriptComponent() {
     <>
       {/* Engineer Script */}
       <Script
-        src="https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/scripts%2F1726642674787-nbl1t6kdlp.js?alt=media&token=0d43f5c3-7e63-4b75-a6a9-68810e888672"
+        src="https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/scripts%2F1726642674787-nbl1t6kdlp-2.js?alt=media&token=bc43690d-07b7-4d39-b6b4-eb738c71f145"
         strategy="lazyOnload"
       />
     </>
