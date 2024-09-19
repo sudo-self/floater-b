25edca031a48e6f5edc9e8c89843baf56eaaca65
Sudo-Self <sudo-self@proton.me>
2024-09-19 00:41:20 -0600

    Update FloaterForm.js

--- app/FloaterForm/FloaterForm.js
+++ app/FloaterForm/FloaterForm.js
@@ -3,7 +3,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import '../../globals.css'; 
+import '../globals.css'; 
 
 const FloaterForm = () => {
     const [tooltipText, setTooltipText] = useState('');
