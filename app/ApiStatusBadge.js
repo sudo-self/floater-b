90bc70a34cd0e374a2226367a507692785d01f62
Sudo-Self <sudo-self@proton.me>
2024-09-19 03:03:52 -0600

    Update ApiStatusBadge.js

--- app/ApiStatusBadge.js
+++ app/ApiStatusBadge.js
@@ -14,7 +14,7 @@ const ApiStatusBadge = () => {
         <img
           src="https://img.shields.io/endpoint?url=https://floater.jessejesse.xyz/api/floater"
           alt="Floater API Status"
-          className="h-8" 
+          className="h-4"
         />
       </a>
     </div>
@@ -22,3 +22,4 @@ const ApiStatusBadge = () => {
 };
 
 export default ApiStatusBadge;
+
