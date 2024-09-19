3f690fa7683fc495739fa53818435b345ef9ec6f
Sudo-Self <sudo-self@proton.me>
2024-09-18 20:53:22 -0600

    Add files via upload

---
+++ app/middlewear/cors.js
@@ -0,0 +1,19 @@
+// middleware/cors.js
+
+export function cors(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  next();
+}
+
+
+
+
+
