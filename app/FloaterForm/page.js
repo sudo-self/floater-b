0e50d72428afbbc4acf1eb400fb8f3abc7120707
Sudo-Self <sudo-self@proton.me>
2024-09-18 21:57:38 -0600

    Add files via upload

---
+++ app/FloaterForm/page.js
@@ -0,0 +1,11 @@
+
+import FloaterForm from './FloaterForm';
+
+export default function FloaterFormPage() {
+  return (
+    <div>
+      <h1>Floater B. APi</h1>
+      <FloaterForm />
+    </div>
+  );
+}
