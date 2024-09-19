6bc99b1d728a3f6e754b3a6455ecb8ded291ba44
Sudo-Self <sudo-self@proton.me>
2024-09-19 02:45:22 -0600

    Create ApiStatusBadge.js

---
+++ app/ApiStatusBadge.js
@@ -0,0 +1,24 @@
+"use client";
+
+import React from 'react';
+
+const ApiStatusBadge = () => {
+  return (
+    <div className="flex items-center">
+      <a
+        href="https://floater.jessejesse.xyz/api/floater"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center"
+      >
+        <img
+          src="https://img.shields.io/endpoint?url=https://floater.jessejesse.xyz/api/floater"
+          alt="Floater API Status"
+          className="h-8" 
+        />
+      </a>
+    </div>
+  );
+};
+
+export default ApiStatusBadge;
