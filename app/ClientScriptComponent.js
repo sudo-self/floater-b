557d7be89ac1f15db2d346d7571d4ba387a0a279
Sudo-Self <sudo-self@proton.me>
2024-09-20 02:22:03 -0600

    Update ClientScriptComponent.js

--- app/ClientScriptComponent.js
+++ app/ClientScriptComponent.js
@@ -5,9 +5,9 @@ import Script from 'next/script';
 export default function ClientScriptComponent() {
   return (
     <>
-      {/* HProjecr Lead */}
+      {/* Rumble */}
       <Script
-        src="https://firebasestorage.googleapis.com/v0/b/jessejessexyz.appspot.com/o/floaterLeft_BG.js?alt=media&token=7c41a90d-283f-4c81-8905-6ee7945397a9"
+        src="https://pub-62f7f17b63fe4f5cbbf739cf66c0c5ee.r2.dev/sksk.js"
         strategy="lazyOnload"
       />
       
