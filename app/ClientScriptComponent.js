a5870c53af49788ff7f9ae7c15edb06b89f8c9b1
Sudo-Self <sudo-self@proton.me>
2024-09-21 11:37:44 -0600

    Update ClientScriptComponent.js

--- app/ClientScriptComponent.js
+++ app/ClientScriptComponent.js
@@ -7,7 +7,7 @@ export default function ClientScriptComponent() {
     <>
       {/* Project Lead */}
       <Script
-        src="https://pub-62f7f17b63fe4f5cbbf739cf66c0c5ee.r2.dev/projectlead.js"
+        src="https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/Project_Lead.js"
         strategy="lazyOnload"
       />
       
