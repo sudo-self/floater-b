ef82580e77d91ca9750ae5c0c59208590f2263e5
Sudo-Self <sudo-self@proton.me>
2024-09-21 01:21:28 -0600

    Update ClientScriptComponent.js

--- app/ClientScriptComponent.js
+++ app/ClientScriptComponent.js
@@ -7,7 +7,7 @@ export default function ClientScriptComponent() {
     <>
       {/* Rumble */}
       <Script
-        src="https://pub-62f7f17b63fe4f5cbbf739cf66c0c5ee.r2.dev/sksk.js"
+        src="https://pub-62f7f17b63fe4f5cbbf739cf66c0c5ee.r2.dev/floater.js"
         strategy="lazyOnload"
       />
       
