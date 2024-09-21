9667d3f98e4a5cbc396c1581b6f9c34dfba084d3
Sudo-Self <sudo-self@proton.me>
2024-09-21 12:57:51 -0600

    Update ClientScriptComponent.js

--- app/ClientScriptComponent.js
+++ app/ClientScriptComponent.js
@@ -11,9 +11,9 @@ export default function ClientScriptComponent() {
         strategy="lazyOnload"
       />
       
-      {/* NFT */}
+      {/* meow */}
       <Script
-        src="https://pub-62f7f17b63fe4f5cbbf739cf66c0c5ee.r2.dev/floaterLEFT.js"
+        src="https://pub-62f7f17b63fe4f5cbbf739cf66c0c5ee.r2.dev/meow.js"
         strategy="lazyOnload"
       />
       
