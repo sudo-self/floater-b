ebb3d167f110e1d1ca8a22363ccf4ad65ea411cf
Sudo-Self <sudo-self@proton.me>
2024-09-21 02:26:05 -0600

    Update ClientScriptComponent.js

--- app/ClientScriptComponent.js
+++ app/ClientScriptComponent.js
@@ -7,7 +7,7 @@ export default function ClientScriptComponent() {
     <>
       {/* source code */}
       <Script
-        src="https://pub-62f7f17b63fe4f5cbbf739cf66c0c5ee.r2.dev/floater.js"
+        src="https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/floater.js"
         strategy="lazyOnload"
       />
       
