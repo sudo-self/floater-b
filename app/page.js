1ec280eb9f30ff29eaaefcc4d8634719a57579a9
Sudo-Self <sudo-self@proton.me>
2024-09-21 03:22:39 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import BigText from './BigText';
 import Footer from './Footer';
 import ColorPicker from './ColorPicker';
 import { useAuth, signInWithGoogle, signInWithGithub, handleSignOut } from './firebase';
-import { generateScriptContent, uploadScript } from './scriptUtils';
+import { generateFileContent, uploadScript } from './scriptUtils';
 import ClientScriptComponent from './ClientScriptComponent';
 
 const Home = () => {
