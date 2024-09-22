dbdf777e8c1b5632fb26ae6708b9c2dbb9bbffd5
Sudo-Self <sudo-self@proton.me>
2024-09-22 01:34:39 -0600

    Update ClientScriptComponent.js

--- app/ClientScriptComponent.js
+++ app/ClientScriptComponent.js
@@ -28,6 +28,13 @@ export default function ClientScriptComponent() {
         src="https://pub-62f7f17b63fe4f5cbbf739cf66c0c5ee.r2.dev/graphics.js"
         strategy="lazyOnload"
       />
+      
+      {/* Domain */}
+      <Script
+        src="https://pub-c1de1cb456e74d6bbbee111ba9e6c757.r2.dev/domain.js"
+        strategy="lazyOnload"
+      />
     </>
   );
 }
+
